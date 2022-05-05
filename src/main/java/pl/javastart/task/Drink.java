296536af9c57bcf715dbfc0094a2bb3e81b276c2
Marcin Kunert <the.martines@gmail.com>
2022-05-05 12:57:52 +0200

    Rozwiązanie

---
+++ src/main/java/pl/javastart/task/Drink.java
@@ -0,0 +1,18 @@
+package pl.javastart.task;
+
+public class Drink {
+    String name;
+    double price;
+    boolean alcohol;
+    String NameFirstIgredient;
+    int quantity1;
+    String NameSecondIgredient;
+    int quantity2;
+    String NameThirdIgredient;
+    int quantity3;
+    int capacity;
+
+
+
+
+}
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,40 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Drink drink = new Drink();
+        drink.name = "Mojito";
+        drink.price = 13.0;
+        drink.alcohol = true;
+        drink.NameFirstIgredient = "Rum";
+        drink.NameSecondIgredient = "Lemon juice";
+        drink.NameThirdIgredient = "Water";
+
+        System.out.println("Drink:" + drink.name);
+        System.out.println("Cena:" + drink.price);
+        System.out.println("Czy drink zawiera alkohol? : " + drink.alcohol);
+        System.out.println("Składniki:");
+        System.out.println(drink.NameFirstIgredient  +  " 50ml ");
+        System.out.println(drink.NameSecondIgredient  +  " 5ml ");
+        System.out.println(drink.NameThirdIgredient  +  " 25ml ");
+
+
+        drink.quantity1 = 50;
+        drink.quantity2 = 5;
+        drink.quantity3 = 25;
+
+        int capacitySum = drink.quantity1 + drink.quantity2 + drink.quantity3;
+        System.out.println("Pojemność: " + capacitySum + "ml");
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
